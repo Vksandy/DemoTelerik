6ae475ca89d381ad892dde7bf8ab6acc98ce6266
Vijay <vijaykumar2907@outlook.com>
2022-11-19 13:58:29 +0530

    InitialCommit

---
+++ DemoTelerik2/Article.cs
@@ -0,0 +1,10 @@
+﻿
+namespace DemoTelerik2
+{
+    public class Article
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
